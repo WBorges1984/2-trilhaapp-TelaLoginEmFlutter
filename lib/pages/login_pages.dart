d99be9329569a4b543a71d81e19a4aa84a957dc7
Willian.Borges <willianodhara@gmail.com>
2023-09-13 23:56:51 -0300

    Initial commit

---
+++ lib/pages/login_pages.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 40, 28, 53),
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 70,
+              ),
+              Row(
+                children: [
+                  Expanded(child: Container()),
+                  Expanded(
+                    flex: 8,
+                    child: Image.network(
+                      "https://hermes.digitalinnovation.one/assets/diome/logo.png",
+                    ),
+                  ),
+                  Expanded(child: Container()),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Já tem Cadastro?",
+                style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Faça seu login e make the change_",
+                style: TextStyle(fontSize: 14, color: Colors.white),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                height: 30,
+                alignment: Alignment.center,
+                child: const Row(
+                  children: [
+                    Expanded(
+                        flex: 2,
+                        child: Text("Informe sua email: ",
+                            style: TextStyle(color: Colors.white))),
+                    Expanded(
+                        flex: 3,
+                        child: Text("Email",
+                            style: TextStyle(color: Colors.white))),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                height: 30,
+                alignment: Alignment.center,
+                child: const Row(
+                  children: [
+                    Expanded(
+                        flex: 2,
+                        child: Text("Informe sua senha: ",
+                            style: TextStyle(color: Colors.white))),
+                    Expanded(
+                        flex: 3,
+                        child: Text("Senha",
+                            style: TextStyle(color: Colors.white))),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                color: Colors.green,
+                height: 30,
+                alignment: Alignment.center,
+                child: const Text("Login"),
+              ),
+              Expanded(child: Container()),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                height: 30,
+                alignment: Alignment.center,
+                child: const Text(
+                  "Esqueci minha senha",
+                  style: TextStyle(color: Colors.yellow),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                height: 30,
+                alignment: Alignment.center,
+                child: const Text(
+                  "Criar conta",
+                  style: TextStyle(
+                      color: Colors.green, fontWeight: FontWeight.w500),
+                ),
+              ),
+              const SizedBox(
+                height: 60,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
